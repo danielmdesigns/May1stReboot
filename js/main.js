1e4028f9d7fc22737c63f9a767892ebccc3513f4
150. ?  <danielwmaldonado@gmail.com>
2016-04-15 12:36:18 -0500

    add description(s)
    add description(s)

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@
 $(document).ready(function(){
 "use strict";
 
+//READ MORE
 $("#js-rm").on("click", function(e){
     e.preventDefault();
     $(".is-hidden").slideToggle(800);
     $(this).text($(this).text() === "Read Less" ? "Read More" : "Read Less");
 });
 
+//GALLERY MOUSE HOVER
 $(".grid li > a > img").hover(function(){
 	$(this).stop().animate({"opacity": "1"}, "slow");
 },function(){
 	$(this).stop().animate({"opacity": "0.7"}, "slow");
 });
 
+//BACK TO TOP
 $("#js-top").on("click", function(){
   if($(window).scrollTop() > 0){
 	$("html, body").animate({scrollTop: 0}, 1500);
